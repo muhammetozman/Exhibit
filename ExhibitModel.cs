3e05bfe39b0d861ebc5b64fa496e44ee9f8ea560
Muhammet Ozman <muhammetozman@anadoluhayat.com.tr>
2023-02-06 00:25:14 +0300

    .

---
+++ ExhibitModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exhibit
+{
+    public class ExhibitModel
+    {
+        public string PLAY_ID { get; set; }
+        public int SONG_ID { get; set; }
+        public int CLIENT_ID { get; set; }
+        public DateTime PLAY_TS { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Exhibit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ExhibitModel> models = new List<ExhibitModel>();
+
+            using (var reader = new StreamReader(@"C:\Users\hymprduser1\Desktop\exhibitA-input.csv"))
+            {
+                reader.ReadLine(); // to seperate header
+                while (!reader.EndOfStream)
+                {
+
+                    var line = reader.ReadLine();
+
+                    string sep = "\t";
+                    var values = line.Split(sep.ToCharArray());
+
+                    models.Add(new ExhibitModel
+                    {
+                        PLAY_ID = values[0],
+                        SONG_ID = Convert.ToInt32(values[1]),
+                        CLIENT_ID = Convert.ToInt32(values[2]),
+                        PLAY_TS = Convert.ToDateTime(values[3])
+                    });
+                }
+
+            }
+
+            var disticntExhhibit = models.Where(x => x.PLAY_TS.ToShortDateString() == "2016-10-08").Select(m => new { m.CLIENT_ID, m.SONG_ID }).Distinct().ToList();
+
+            var result = disticntExhhibit.GroupBy(x => new { x.CLIENT_ID }).Select(gcs => new
+            {
+                ClientId = gcs.Key.CLIENT_ID,
+                TotalCount = gcs.Count()
+            }).GroupBy(x => new { x.TotalCount }).Select(gcs => new
+            {
+                DistinctPlayCount = gcs.Key.TotalCount,
+                ClientCount = gcs.Count()
+            }).OrderBy(x=>x.DistinctPlayCount).ToList();
+
+
+            //to write a csv file for output
+
+            var filepath = @"C:\Users\hymprduser1\Desktop\exhibitA-output.csv";
+            using (StreamWriter writer = new StreamWriter(new FileStream(filepath,
+            FileMode.Create, FileAccess.Write)))
+            {
+                
+                writer.WriteLine("DISTINCT_PLAY_COUNT,CLIENT_COUNT");
+                foreach (var item in result)
+                {
+                    writer.WriteLine($"{item.DistinctPlayCount},{item.ClientCount}");
+                }
+            }
+
+        }
+    }
+}
